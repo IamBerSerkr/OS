9ead619e48abd2e55c2ca5fbffefe4f13c7b4062
IamBerSerkr <ivan.leonov@live.com>
2022-05-23 22:53:48 +0300

    Process creation completed

--- LAB_4/C++98/receiver.cpp
+++ LAB_4/C++98/receiver.cpp
@@ -1,8 +1,15 @@
 #include <cstdio>
 #include <cstring>
+#include <cwchar>
+#include <cstdlib>
+#include <cerrno>
+#include <exception>
 #include <Windows.h>
 
-const char* binaryFileExtention = ".bin";
+const char* kBinaryFileExtention = ".bin";
+const wchar_t* kSenderExe =  L"sender.exe";
+
+HANDLE START_PROCESS(const wchar_t*);
 
 int main()
 {
@@ -23,9 +30,15 @@ int main()
         }
     } while (numberOfRecords <= 0);
 
-    char *path = nullptr;
+    char path[100 + 4 + 1];
+    int len = sprintf_s(path, "%s.bin", binFileNameBuffer);
+    if (len == -1)
+    {
+        perror("Error! Failed creating a path string!\n");
+        return ECANCELED;
+    }
     strcpy(path, binFileNameBuffer);
-    strcat(path, binaryFileExtention);
+    strcat(path, kBinaryFileExtention);
     FILE *fptr = fopen(path, "wb"); 
     fclose(fptr);
 
@@ -45,8 +58,85 @@ int main()
     HANDLE* senderHandle = (HANDLE*)malloc(sizeof(HANDLE) * numberOfProcesses); 
 
 
-    HANDLE mutex = CreateMutex(NULL, FALSE, L"MUTEX");
+    HANDLE mutex = CreateMutex(NULL, FALSE, "MUTEX");
+    HANDLE writeSemaphore = CreateSemaphore(NULL, numberOfRecords, numberOfRecords, "WRITE_SEMAPHORE");
+    HANDLE readSemaphore = CreateSemaphore(NULL, 0, numberOfRecords, "READ_SEMAPHORE");
     
+   if ((mutex && writeSemaphore && readSemaphore) == NULL)
+   {
+       perror("Error! Failed creating synchronization objects\n");
+       return GetLastError();
+   } 
+
+    // create processes
+    HANDLE* readinessFlag = (HANDLE*)malloc(sizeof(HANDLE) * numberOfProcesses);
+
+    wchar_t wcharPathBuffer[256];
+    mbstowcs(wcharPathBuffer, path, 256);
+    for (int i = 0; i < numberOfProcesses; i++)
+    {
+        wchar_t commandLineArguments[250];
+        len = swprintf_s(commandLineArguments, L"%s %s %d %d", 
+            kSenderExe, wcharPathBuffer, numberOfRecords, i);
+        if (len == -1)
+        {
+            perror("Error! Failed creating a command line arguments string!\n");
+            return ECANCELED;
+        }
+        // wprintf_s(L"%i: %ls\n", i, commandLineArguments);
+
+        // creating ready 
+        char readinessEventName[256];
+        len = sprintf_s(readinessEventName, "%d READY", i);
+        if (len == -1)
+        {
+            perror("Error! Failed creating a readiness event name string!\n");
+            return ECANCELED;
+        }
+        HANDLE ready = CreateEvent(NULL, FALSE, FALSE, readinessEventName);
+        if (ready == NULL)
+        {
+            perror("Error! Failed creating event!\n");
+            return GetLastError();
+        }
+        readinessFlag[i] = ready; 
+
+        HANDLE hProcess = START_PROCESS(commandLineArguments);
+        if (!hProcess)
+        {
+            perror("Error! Failed to start a process!\n");
+            return GetLastError();
+        }
+        senderHandle[i] = hProcess;
+    }
+
+    // wait for all processes
+    WaitForMultipleObjects(numberOfProcesses, readinessFlag, TRUE, INFINITE);
+
+    free(readinessFlag);
+    CloseHandle(mutex);
+    CloseHandle(writeSemaphore);
+    CloseHandle(readSemaphore);
     free(senderHandle);
   
     return 0;
+}
+
+HANDLE START_PROCESS(const wchar_t* args)
+{
+    STARTUPINFO startupInfo;
+    PROCESS_INFORMATION processInformation;
+
+    ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
+    startupInfo.cb = sizeof(startupInfo);
+
+    if (!CreateProcess(NULL, args, NULL, NULL, FALSE,
+            CREATE_NEW_CONSOLE, NULL, NULL, &startupInfo, &processInformation))
+    {
+        return NULL;
+    }
+
+    CloseHandle(processInformation.hThread);
+    return processInformation.hProcess;
+}
+
